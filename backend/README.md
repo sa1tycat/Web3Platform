9456c8ee1f81fe92c87fb3c25a854cb348ac4f6d
sa1tycat <yangfuming@stud.tjut.edu.cn>
2023-11-26 21:10:23 +0800

    Set up basic structure for app.js

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+To run the backend server, type `node app.js` in terminal of current directory, and enter.
---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express'); // 引入express模块
+const app = express();
+
+app.use(express.json()); // 用于解析JSON格式的请求体
+app.use(express.urlencoded({ extended: true })); // 用于解析表单格式的请求体
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// 错误处理
+app.use((err, req, res, next) => {
+  res.status(500).send('Something broke!');
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
