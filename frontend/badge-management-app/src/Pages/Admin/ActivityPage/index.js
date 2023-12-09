99e418eb4483706914b67c28e486814e7a95dd62
w3ngjin <w320656@163.com>
2023-12-09 19:54:21 +0800

    实现获取活动api

--- frontend/badge-management-app/src/Pages/Admin/ActivityPage/index.js
+++ frontend/badge-management-app/src/Pages/Admin/ActivityPage/index.js
@@ -1,62 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Button, Row, Col } from 'antd';
 import moment from 'moment'; // 用于格式化日期
-// import 'antd/dist/antd.css'; // 确保Ant Design样式被导入
+import { getActivity } from '../../../API';
 
-// 您直接定义的JSON数据中的活动数组
-const activitiesData = [
-    // ...这里是您的活动数据
-    {
-        "success": true,
-        "activity": [
-            {
-                "ActivityID": 1,
-                "Name": "测试活动",
-                "Description": "test",
-                "StartTime": "2023-11-10T16:00:00.000Z",
-                "EndTime": "2023-12-11T16:00:00.000Z",
-                "CreatedAt": "2023-11-28T01:06:07.000Z",
-                "UpdatedAt": "2023-11-28T01:06:07.000Z"
-            },
-            {
-                "ActivityID": 2,
-                "Name": "测试活动2",
-                "Description": "测试2",
-                "StartTime": "2023-10-22T16:00:00.000Z",
-                "EndTime": "2023-12-24T16:00:00.000Z",
-                "CreatedAt": "2023-11-28T02:54:03.000Z",
-                "UpdatedAt": "2023-11-28T02:54:23.000Z"
-            },
-            {
-                "ActivityID": 3,
-                "Name": "post上传测试活动",
-                "Description": "test",
-                "StartTime": "2023-11-09T16:00:00.000Z",
-                "EndTime": "2023-12-11T16:00:00.000Z",
-                "CreatedAt": "2023-11-29T00:57:43.000Z",
-                "UpdatedAt": "2023-11-29T00:57:43.000Z"
-            },
-            {
-                "ActivityID": 4,
-                "Name": "书法大赛",
-                "Description": "为了提高大家书写，特此举办书法大赛。",
-                "StartTime": "2023-10-09T16:00:00.000Z",
-                "EndTime": "2023-12-21T16:00:00.000Z",
-                "CreatedAt": "2023-12-05T02:41:16.000Z",
-                "UpdatedAt": "2023-12-05T02:41:16.000Z"
-            }
-        ],
-        "message": "Activities retrieved successfully"
-    }
-];
 
-const ActivityCards = () => {
-  // 从JSON数据中提取activity数组
-  const activities = activitiesData[0].activity;
+const ActivityPage = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    getActivity().then(activitiesArray => {
+      setActivities(activitiesArray); // 使用返回的数组更新状态
+    });
+  }, []);
 
   return (
     <Row gutter={[16, 16]} justify="start">
-      {activities.map((activity) => (
+      {activities.length > 0 ? activities.map((activity) => (
         <Col key={activity.ActivityID} xs={24} sm={12} lg={8} xl={6}>
           <Card
             hoverable
@@ -78,9 +37,9 @@ const ActivityCards = () => {
             />
           </Card>
         </Col>
-      ))}
+      )) : <p>Loading activities...</p>} {/* 当活动数组为空时显示加载提示 */}
     </Row>
   );
 };
 
-export default ActivityCards;
+export default ActivityPage;
