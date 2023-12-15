3b60e0c55abb5c93dec091fb36530ef054f83a3d
w3ngjin <w320656@163.com>
2023-12-15 12:59:09 +0800

    增加不同userID的徽章

--- frontend/alumni-app/src/Pages/BadgePage/index.js
+++ frontend/alumni-app/src/Pages/BadgePage/index.js
@@ -1,10 +1,54 @@
-import { Typography } from "antd";
+import React, { useState, useEffect } from 'react';
+import { Card, Row, Col } from 'antd';
+import { useSearchParams } from 'react-router-dom';
+import  getBadges  from '../../API/getBadges'; // 假设这是获取徽章数据的API
 
-function ActivityPage() {
-    return (
-        <div>
-            <Typography.Title level={4}>ActivityPage</Typography.Title>
+const BadgesPage = () => {
+  const [badges, setBadges] = useState([]);
+  const [searchParams] = useSearchParams();
+  const userID = searchParams.get('userID');
+
+  useEffect(() => {
+    if (userID) {
+      getBadges(userID).then(data => {
+        if (data && data.success) {
+          setBadges(data.badges);
+        } else {
+          // 处理没有数据的情况
+        }
+      });
+    }
+  }, [userID]);
+
+  const cardStyle = {
+    width: '100%',
+    textAlign: 'center',
+    minHeight: '100%', // 设置最小高度为100%
+  };
+
+  return (
+    <Row gutter={[16, 16]} justify="start" align="stretch"> {/* 保持样式一致性 */}
+      {badges.length > 0 ? badges.map((badge) => (
+        <Col key={badge.badgeID} xs={24} sm={12} lg={8} xl={6}>
+          <Card
+            hoverable
+            style={cardStyle} // 应用自定义样式
+            cover={<img alt="badge" src={badge.imageURL} />} // 显示图片
+          >
+            <Card.Meta
+              title={badge.title}
+              description={badge.description}
+            />
+          </Card>
+        </Col>
+      )) : (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+          {/* 加载指示器或空状态提示 */}
         </div>
-    );
-}
-export default ActivityPage;
+      )
+      }
+    </Row>
+  );
+};
+
+export default BadgesPage;
