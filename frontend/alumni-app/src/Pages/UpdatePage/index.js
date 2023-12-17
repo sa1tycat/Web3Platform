e76907058f1d13dcedf1a4d295ac91fbae33bc3e
w3ngjin <w320656@163.com>
2023-12-17 22:43:21 +0800

    没用删除

--- frontend/alumni-app/src/Pages/UpdatePage/index.js
+++ frontend/alumni-app/src/Pages/UpdatePage/index.js
@@ -4,8 +4,6 @@ function UpdatePage() {
     return (
         <div>
             <Typography.Title level={4}>UpdatePage</Typography.Title>
-            <iframe width="290" height="150" src="https://www.youtube.com/embed/dqY3mm3DZJA" title="[Preview] NewJeans (뉴진스) 2024 SEASON&#39;S GREETINGS Preview Clip #2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            <iframe width="290" height="150" src="//player.bilibili.com/player.html?aid=877064857&bvid=BV1PN4y1Y7Fw&cid=1362543345&p=1" title="[Preview] NewJeans (뉴진스) 2024 SEASON&#39;S GREETINGS Preview Clip #2" frameborder="0" allowfullscreen></iframe>
         </div>
     );
 }
