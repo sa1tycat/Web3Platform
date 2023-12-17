a1bb828200ed4f5b1b41e0cd7762d8b4d83ac391
w3ngjin <w320656@163.com>
2023-12-17 18:14:04 +0800

    不影响使用

--- frontend/alumni-app/src/Pages/ChatbotPage/components/AppHeader.js
+++ frontend/alumni-app/src/Pages/ChatbotPage/components/AppHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Typography, Row, Col } from 'antd';
+import { BackwardOutlined } from '@ant-design/icons';
 import styles from '../styles/AppHeader.module.css';
 
 const { Title } = Typography;
@@ -7,7 +8,7 @@ const { Title } = Typography;
 const AppHeader = ({ onBack }) => {
   return (
     <div className={styles.header}>
-      <Button onClick={onBack} className={styles.backButton}>返回</Button>
+      <Button onClick={onBack} className={styles.backButton}><BackwardOutlined /></Button>
       <Row justify="center" align="middle">
         <Col>
           <Title level={2} className={styles.fancyTitle}>Gemini_Ai</Title>
