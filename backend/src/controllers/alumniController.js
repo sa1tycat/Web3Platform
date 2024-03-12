86b1cf9574918fcf8f9a365c58f86b1ea7048079
sa1tycat <yangfuming@stud.tjut.edu.cn>
2024-03-12 16:09:43 +0800

    修改userID参数获取方式

--- backend/src/controllers/alumniController.js
+++ backend/src/controllers/alumniController.js
@@ -39,7 +39,7 @@ const viewBadges = async (req, res) => {
 // 查看活动
 const viewActivities = async (req, res) => {
   try {
-    const userID = parseInt(req.query.userID); // 获取userID并转换为整数
+    const { userID } = req.query;
     const activities = await activityService.viewActivitiesWithStatus(userID);
 
     res.json({
